b797f0e60dd7393ad928c35f8ac4c6365d98fba7
Miroslav Vincevich <slavav9779@gmail.com>
2017-04-13 15:16:38 +0000

    added paragraph

--- js/index.js
+++ js/index.js
@@ -1,31 +1,18 @@
-$(function(){
-$("#btn-submit").click(sendForm);
-});
-
-function sendForm(e){
-e.preventDefault();
-
-$.ajax({
-url: "https://formspree.io/slavav9779@gmail.com", 
-method: "POST",
-data: {
-clientName: $("#text").val(),
-clientEmail:$("#email").val(),
-comment:$("#message").val()
-},
-dataType: "json"
-}).done(function(){
-$("#done").html("Дякуємо, ми обов'язково розглянемо ваше повідомлення, та надамо відповідь.");
-}
-);
-
-}
-
-document.querySelector("input[type=submit]").addEventListener("click",sendForm);
+document.querySelector("input[type=submit]").addEventListener("click", sendForm);
+document.getElementById('myform').reset();
 function sendForm(e) {
-var form = document.querySelector("form");
-if(form.checkValidity()){
-e.preventDefault();
-alert("Write code to send form here");
-}
+    var form = document.querySelector("form");
+    if (form.checkValidity()) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/slavav9779@gmail.com",
+            method: "POST",
+            data: {
+                clientName: $("#text").val(),
+                clientEmail: $("#email").val(),
+                comment: $("#message").val()
+            },
+            dataType: "json"
+        });
+    }
 }
